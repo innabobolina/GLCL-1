27c9616ef01017b410d0451913ad456be5ddfef0
Inna <innabobolina@yahoo.com>
2018-09-02 01:43:09 -0700

    modified tracker, buttons and demo; 15min functionality

--- tracker.js
+++ tracker.js
@@ -1,13 +1,35 @@
 let tracker = () => {
 
-	let min5 = document.getElementById('5-min')
+	let plus5 = document.getElementById('plus-5');
 	let counter = document.getElementById('counter');
-	let i = 0;
-	min5.addEventListener('click', ()=>{
-		i += 5;
-		counter.innerHTML = i;
+	let totalTime = 0;
+	const timeInADay = 1440;
+	plus5.addEventListener('click', ()=>{
+		if (totalTime < timeInADay) {
+			totalTime += 5;
+			counter.innerHTML = totalTime;
+		}
 	});
-	console.log(min5);
+	let minus5 = document.getElementById('minus-5');
+	minus5.addEventListener('click', ()=>{
+		if (totalTime > 0) {
+			totalTime -= 5;
+			counter.innerHTML = totalTime;
+		}
+	});
+	let plus15 = document.getElementById('plus-15');
+	plus15.addEventListener('click', ()=>{
+		if (totalTime < timeInADay) {
+			totalTime += 15;
+			counter.innerHTML = totalTime;
+		}
+	});
+	let minus15 = document.getElementById('minus-15');
+	minus15.addEventListener('click', ()=>{
+		if (totalTime > 0) {
+			totalTime -= 15;
+			counter.innerHTML = totalTime;
+		}
+	});
 };
-
 document.addEventListener('DOMContentLoaded', tracker, false);
